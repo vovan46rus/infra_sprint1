8da2390e28301daee0c67714af3b64b0472484a5
vovan46rus <punk.potap@gmail.com>
2023-08-08 21:15:18 +0000

    ...

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -1,14 +1,12 @@
 import os
 from pathlib import Path
 
-dotenv_path = os.path.join(os.path.dirname(__file__), '.env')
-load_dotenv(dotenv_path)
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = 'SECRET_KEY'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
-DEBUG = 'DJANGO_DEBUG', ''.lower() == 'false'
+DEBUG = os.getenv("DJANGO_DEBUG", "").lower() == "true"
 
 allowed_hosts = os.environ.get('ALLOWED_HOSTS', 'localhost').split(',')
 
