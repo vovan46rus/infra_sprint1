80541ede423e6248ba0a2109d3efb32f27ac9639
vovan46rus <punk.potap@gmail.com>
2023-08-08 08:03:27 +0000

    debug

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -6,12 +6,9 @@ load_dotenv()
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 SECRET_KEY = os.getenv('SECRET_KEY')
 
-DEBUG = False
-
-debug_mode = os.getenv("DEBUG", "false").lower() == "true"
+DEBUG = os.getenv("DJANGO_DEBUG", "").lower() == "true"
 
 allowed_hosts = os.environ.get('ALLOWED_HOSTS', 'localhost').split(',')
 
