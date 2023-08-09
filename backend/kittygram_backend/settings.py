d8bec770b7c4b66592c5c0dae974a3de153ad45e
vovan46rus <punk.potap@gmail.com>
2023-08-09 09:55:46 +0000

    hw

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -1,6 +1,8 @@
 import os
 from pathlib import Path
 
+env_path = Path(__file__).resolve().parent.parent / '.env'
+load_dotenv(dotenv_path=env_path)
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
