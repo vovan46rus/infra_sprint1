88c9478c7230cd70c2b5b5b55c4c199f34a5aa8e
vovan46rus <punk.potap@gmail.com>
2023-08-09 11:47:01 +0000

    hw

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -1,7 +1,8 @@
+import sys
 import os
 from pathlib import Path
 
-env_path = Path(__file__).resolve().parent.parent / 'backend' / 'kittygram_backend' / '.env'
+env_path = Path(__file__).resolve().parent.parent / 'kittygram_backend' / '.env'
 
 with open(env_path) as f:
     for line in f:
